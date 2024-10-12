b1fb8c77410d1105af26d894969cb8731b59f456
Mateus Matos <98349389+mfm051@users.noreply.github.com>
2024-10-12 15:54:15 -0300

    Adiciona menu com opção para aluno

--- main.c
+++ main.c
@@ -1,1 +1,29 @@
-// Teste do rspec
+#include <stdio.h>
+
+int main() {
+    printf("Projeto escola\n\n");
+
+    int opcao;
+    int sair = 0; // falso
+
+    while (!sair) {
+        printf("Opções:\n");
+        printf("0: sair\n");
+        printf("1: aluno\n");
+
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 0: {
+                sair = 1;
+                break;
+            }
+            case 1: {
+                printf("Módulo Aluno\n\n");
+                break;
+            }
+            default:
+                printf("Opção inválida");
+        }
+    }
+}
