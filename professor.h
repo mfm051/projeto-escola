f6171e330bb621d4e06dd78c542cc0f116b51663
Natielly <na7ielly@gmail.com>
2024-10-20 18:47:18 +0000

    Tirando a "disciplina" da struct professor

--- professor.h
+++ professor.h
@@ -2,12 +2,10 @@
 
 #define MAXNOME 100
 #define MAXCPF 11
-#define MAXDISCPILINA 100
 
 typedef struct {
   int ativo;
   int matriculaProfessor;
-  char disciplina[MAXDISCPILINA];
   char nome[MAXNOME];
   char sexo;
   Data data_nascimento;
@@ -20,4 +18,3 @@ int atualizarProfessor(int qtdProfessores, Professor professores[]);
 int excluirProfessor(int qtdProfessores, Professor professores[]);
 int obtemOpcaoProfessor();
 int encontrarMatricula(int qtdProfessores, Professor professores[], int matriculaProfessor);
-int encontrarDisciplina(int qtdProfessores, Professor professores[], char disciplina[]);
