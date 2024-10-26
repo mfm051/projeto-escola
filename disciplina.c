7aed7a37748863c6f2380424c5cabeed1006b9b4
na7ielly <na7ielly@gmail.com>
2024-10-25 21:54:20 -0300

    Completar disciplina.c

--- disciplina.c
+++ disciplina.c
@@ -62,3 +62,58 @@ int cadastrarDisciplina(int qtdDisciplinas, Disciplina disciplinas[], int maxDis
     return VERDADEIRO;
 }
 
+int excluirDisciplina(int qtdDisciplinas, Disciplina disciplinas[]) {
+    int codigo;
+    int posicaoDisciplina;
+
+    printf("Código da disciplina: ");
+    scanf("%d", &codigo);
+
+    if (codigo <= 0) {
+        printf("Código inválido\n");
+        return FALSO;
+    }
+
+    posicaoDisciplina = encontraCodigoDisciplina(qtdDisciplinas, disciplinas, codigo);
+
+    if (posicaoDisciplina == -1) {
+        printf("Disciplina não encontrada\n");
+        return FALSO;
+    }
+    else
+    {
+        disciplinas[posicaoDisciplina].ativo = FALSO;
+
+        for (int i = posicaoDisciplina; i < qtdDisciplinas; i++) {
+            disciplinas[i] = disciplinas[i + 1];
+        };
+
+        return VERDADEIRO;
+    }
+}
+
+int obtemOpcaoDisciplina(){
+    int opcao;
+
+    printf("\nOpções Disciplina:\n");
+    printf("0: voltar\n");
+    printf("1: cadastrar disciplina\n");
+    printf("2: listar disciplinas\n");
+    printf("3: atualizar disciplina\n");
+    printf("4: excluir disciplina\n");
+
+    scanf("%d", &opcao);
+
+    return opcao;  
+}
+
+int encontraDisciplinaProfessor(int qtdDisciplinas, Disciplina disciplinas[], char nomeDisciplina[]) {
+    int posicaoDisciplina = -1;
+
+    for (int i = 0; i < qtdDisciplinas; i++) {
+        if (disciplinas[i].nomeDisciplina == nomeDisciplina && disciplinas[i].ativo)
+            posicaoDisciplina = i;
+    }
+    
+    return posicaoDisciplina;
+}
